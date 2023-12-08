1e0b6268a8d02b9a99493feb7129a6a623324e0c
Reem-degais <reem.degais.33@gmail.com>
2023-12-08 01:10:27 +0000

    updated code style

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -5,7 +5,7 @@ argument and returns the string representation of the float.
 """
 
 
-def to_str (n: float) -> str:
+def to_str(n: float) -> str:
     """takes a float n as argument and returns
     the string representation of the float."""
 
