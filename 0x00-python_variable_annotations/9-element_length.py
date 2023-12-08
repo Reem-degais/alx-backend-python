532cae702599ee229b7fffd11f5cd4cf7b784eef
Reem-degais <reem.degais.33@gmail.com>
2023-12-08 02:39:10 +0000

    create 9-element_length.py file

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Annotate the below function’s parameters and
+return values with the appropriate types
+
+def element_length(lst):
+    return [(i, len(i)) for i in lst]
+"""
+
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Annotate the below function"""
+    return [(i, len(i)) for i in lst]
